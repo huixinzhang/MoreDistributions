8901e1b06746fced196bd5248b37c66496a7c611
HuixinZhang <hxzhang2013@sjtu.edu.cn>
2022-04-08 16:50:29 -0400

    re21

--- src/MoreDistributions.jl
+++ src/MoreDistributions.jl
@@ -1,7 +1,7 @@
 module MoreDistributions
 
 using Distributions, LinearAlgebra, LazyArrays, SpecialFunctions, StatsBase,
-      Random, NLopt, JLD
+      Random, NLopt
 
 # functions to extend
 import Base: convert, rand, maximum, minimum, extrema
